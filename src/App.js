ae1a2bfc6f42abefb4ab8a98f8b4c0b82ecfc41b
Tony W <bobacrypto2@gmail.com>
2023-04-23 18:06:20 -0700

    Practice useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+
+  // componentDidMount() {
+  //   document.title = `You clicked ${this.state.count} times`;
+  // }
+  // componentDidUpdate() {
+  //   document.title = `You clicked ${this.state.count} times`;
+  // }
+  
+  // Is the same as:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`
+  });
 
   return (
     <div>
